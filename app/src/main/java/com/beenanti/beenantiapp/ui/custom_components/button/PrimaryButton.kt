004a81245f960ed0bebd95d6447aeaddbba0dbf8
Akmal Rafi Nursyabani <akmalrafi.yourdev@gmail.com>
2023-03-18 20:32:47 +0700

    Login Screen dan membuat styling pada text

--- app/src/main/java/com/beenanti/beenantiapp/ui/custom_components/button/PrimaryButton.kt
+++ app/src/main/java/com/beenanti/beenantiapp/ui/custom_components/button/PrimaryButton.kt
@@ -1,10 +1,18 @@
 package com.beenanti.beenantiapp.ui.custom_components.button
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.view.Gravity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import com.beenanti.beenantiapp.R
 
 class PrimaryButton: AppCompatButton {
+
+    private lateinit var enabledColorBackground: Drawable
+    private lateinit var disabledColorBackground: Drawable
 
     constructor(context: Context) : super(context) {
         init()
@@ -21,5 +29,17 @@ class PrimaryButton: AppCompatButton {
         init()
     }
 
-    private fun init() {}
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        background = if (isEnabled) enabledColorBackground else disabledColorBackground
+
+        textSize = 16f
+        gravity = Gravity.CENTER
+        setTextColor(ContextCompat.getColor(context, android.R.color.black))
+    }
+
+    private fun init() {
+        enabledColorBackground = ContextCompat.getDrawable(context, R.drawable.button_background) as Drawable
+        disabledColorBackground = ContextCompat.getDrawable(context, R.drawable.button_backgroud_disable) as Drawable
+    }
 }
