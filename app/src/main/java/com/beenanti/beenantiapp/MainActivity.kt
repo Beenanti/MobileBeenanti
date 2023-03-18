a713cc3b56be770824dfa67b1de09d822fd732b3
h4ckm3 <77613381+h4ckm3@users.noreply.github.com>
2023-03-18 16:32:36 +0700

    pembuatan profil sudah login

--- app/src/main/java/com/beenanti/beenantiapp/MainActivity.kt
+++ app/src/main/java/com/beenanti/beenantiapp/MainActivity.kt
@@ -6,14 +6,14 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnmulai : Button
+//    private lateinit var btnmulai : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_launchscreen)
-        btnmulai = findViewById(R.id.btn_mulai)
-        btnmulai.setOnClickListener{
-            val intent = Intent(this@MainActivity, ProfilblmloginActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.profilsudahlogin)
+//        btnmulai = findViewById(R.id.btn_mulai)
+//        btnmulai.setOnClickListener{
+//            val intent = Intent(this@MainActivity, ProfilblmloginActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
